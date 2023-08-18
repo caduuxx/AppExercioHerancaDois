e240e9d897d7b3d9f42ed5e6b65775fef417eb35
caduuxx <115509827+caduuxx@users.noreply.github.com>
2023-08-18 14:35:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using AppExercioHerancaDois.Dominio;
+using System.Runtime.CompilerServices;
+
+Animal animal = new Animal();
+Cachorro cachorro = new Cachorro();
+Gato gato = new Gato();
+
+Console.WriteLine("qual a raça do seu cachorro");
+cachorro.Raça = Console.ReadLine();
+Console.WriteLine("qual o nome do seu Cachorro");
+cachorro.Nome = Console.ReadLine();
+
+Console.WriteLine("qual a raça do seu gato");
+gato.Raça = Console.ReadLine();
+Console.WriteLine("qual o nome do seu gato");
+gato.Nome = Console.ReadLine();
+
